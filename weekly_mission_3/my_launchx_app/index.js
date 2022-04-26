619b0c56a6995e9b204b829c5aae0dfab6e38644
Octavio Olea <oleaoctavio23@gmail.com>
2022-04-26 15:30:06 -0500

    Modificaciones para enviar mis repos.

--- weekly_mission_3/my_launchx_app/index.js
+++
@@ -1,8 +0,0 @@
-const MissionCommander = require('./app/missionCommander');
-const octavio = new MissionCommander("octavio");
-const pedro = new MissionCommander("pedro");
-const josue = new MissionCommander("josue");
-
-console.log(octavio.name);
-console.log(pedro.name);
-console.log(josue.name);
